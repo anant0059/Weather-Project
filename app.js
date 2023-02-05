d235452098860ea0b9b7828d8a9c11799452d074
anant0059 <2019uce0059@gmail.com>
2023-02-05 12:04:03 +0530

    reading input city

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=494e541d8491f73b716cc6260a480714";
+
+    res.sendFile(__dirname + "/index.html");
+
+    //res.send("Server is up and running.")
+});
+
+app.post("/", function(req, res){
+    console.log(req.body.cityName);
+
+    const query = req.body.cityName;
+    const apikey = "494e541d8491f73b716cc6260a480714";
+    const unit = "metric";
+    
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + unit;
     https.get(url, function(response){
         console.log(response.statusCode);
 
@@ -24,13 +40,12 @@ app.get("/", function(req, res){
             // }
             // console.log(JSON.stringify(object));
             res.write("<p>The weather is currently " + weatherDescription + ".</p>");
-            res.write("<h1>The temperature in London is " + temp + " Kelvin.</h1>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees celcius.</h1>");
             res.write("<img src=" + imageURL + ">");
             res.send();
         });
     });
-    //res.send("Server is up and running.")
-});
+})
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000.");
